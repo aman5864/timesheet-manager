b14ba5e9b7b42a17bcd9b6751dc3d76f58bd0a33
aman.khan <amankhan@mastersindia.co>
2025-08-04 17:51:31 +0530

    added guinocorn and updated setting file

--- timesheet/settings.py
+++ timesheet/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#j3uqtmf%40z6b2m!p!x5mb7l^-m0*5kv^01+zxcqagj8ws33*'
+SECRET_KEY = SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -82,14 +83,7 @@ WSGI_APPLICATION = 'timesheet.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'mysql.connector.django',
-        'NAME': 'timesheet',
-        'USER': 'aman',
-        'PASSWORD': 'aman123',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
+    'default': dj_database_url.parse(config('DATABASE_URL'))
 }
 
 
